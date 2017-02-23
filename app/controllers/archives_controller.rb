3b8645845a231a946860fbd8e15c402dc9a48f95
Judy Lin <julin@hmc.edu>
2017-02-22 19:52:51 -0800

    finish navigation

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,4 +1,19 @@
 class ArchivesController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def expo
+  end
+
+  def media
+  end
+
+  def contact
+  end
 end
