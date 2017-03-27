3b56d603718d795bfcd96cc82afa35a2069358f9
Daniel <daniel.sonner@gmail.com>
2017-03-26 20:16:41 -0700

    Now searches by tag
    Now searches by tag in addition to description and title.
    Unfortunately had to be a bit hacky in implementation because
    or did not behave as expected, see comments for details.

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -3,7 +3,7 @@ class ArchivesController < ApplicationController
     @archives = Archive.paginate(:page => params[:page], :per_page => 10)
     if params[:search]
       @archives = Archive.search(params[:search])
-        .order("created_at DESC")
+        .order("updated_at DESC")
         .paginate(:page => params[:page], :per_page => 10)
     end
   end
--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,12 +2,29 @@ class Archive < ApplicationRecord
   def self.search(search)
     search_length = search.split.length
     if search_length > 0
-      where([(["title LIKE ? OR description LIKE ?"] * search_length).join(' OR ')] \
-              + search.split.map{ |name| "%#{name}%" } \
-              + search.split.map{ |name| "%#{name}%" })
+      # had to be very hacky in this if since I could not .or the result of a
+      # mysql where which I wrote with the result of a tagged_with despite them
+      # having the same structure so far as I could tell from extensive testing.
+      # Was getting a ArgumentError that they must be structurally compatable
+      # and that they had incompatable values
+
+      # find the where section that the ActsAsTaggableOn gem generates
+      tag_where = tagged_with(search.split, :any => true).to_sql.partition("WHERE").last
+
+      # generate a list of [searchterm1, searchterm1, searchterm2, searchterm2, ...]
+      individual_search_terms_x2 = search.split.flat_map{ |name| ["%#{name}%", "%#{name}%"] }
+
+      # create the where query which will combine searching title, description,
+      # and the where term given by ActsAsTaggableOn joining by OR to display
+      # the maximal possible matches
+      where_term = [(["title LIKE ? OR description LIKE ?"] * search_length + [tag_where]).join(' OR ')] \
+              + individual_search_terms_x2
+      where(where_term)
     else
+      # no search terms so display all. This seems like easiest way to select all
       where("title LIKE ?", "%#{search}%")
     end
+
   end
   acts_as_taggable
   has_attached_file :media, :styles => {:thumb => {:geometry => "250x250#", :format => 'jpg', :time => 0}}, :default_url => "/images/missing.png"
